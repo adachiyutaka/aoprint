796cf184de3099c828ef22bd2919421d8a90cb9d
adachiyutaka <adachi.yutaka@gmail.com>
2020-08-22 16:09:33 +0900

    GameFormの保存処理にGameObject, Position, Stage, Lineを追加

--- app/forms/game_form.rb
+++ app/forms/game_form.rb
@@ -7,9 +7,9 @@ class GameForm
 
   def save
     game = Game.create(name: name, text: text)
-    # gameObject = GameObject.create(images: player, game_id: game.id)
-    # Position.create()
-    # stage = Stage.create()
-    # Line.create()
+    gameObject = GameObject.create(images: player, game_id: game.id)
+    Position.create(x: 0, y: 0, game_object_id: gameObject.id)
+    stage = Stage.create(images: stage, game_id: game.id)
+    Line.create(s_x: 0, s_y: 0, e_x: 0, e_y: 0, stage_id: stage.id)
   end
 end
