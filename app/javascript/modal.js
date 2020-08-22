c1bca47854a3932c64792c6986fdfda0c034ec04
adachiyutaka <adachi.yutaka@gmail.com>
2020-08-22 14:50:02 +0900

    モーダルの作成

---
+++ app/javascript/modal.js
@@ -0,0 +1,17 @@
+const modal = () => {
+  const button = document.getElementById('create-btn');
+  const modalBackground = document.getElementById('modal-background');
+  const exit = document.getElementById('exit-btn');
+
+  
+  button.addEventListener('click', () => {
+    console.log('create-btn');
+    modalBackground.style.display = 'block';
+  });
+  
+  exit.addEventListener('click', () => {
+    modalBackground.style.display = 'none';
+  });
+}
+
+window.addEventListener("load", modal)
