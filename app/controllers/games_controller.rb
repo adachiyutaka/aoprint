bf13990fb77385a358afb2ea3d6d35f92929162c
adachiyutaka <adachi.yutaka@gmail.com>
2020-08-22 18:30:06 +0900

    game#createで作成後にgameページに遷移するよう設定

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,12 +3,15 @@ class GamesController < ApplicationController
   # def index
   #   @game = GameForm.new
   # end
+  def show
+    @game = Game.find_by(id: params[:id])
+  end
 
   def create
     @game = GameForm.new(game_params)
     if @game.valid?
       @game.save
-      return redirect_to root_path
+      return redirect_to game_path(@game.game)
     else
       render "new"
     end
