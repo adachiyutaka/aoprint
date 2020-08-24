ceeb52d79f1bd3aa0c005c2945ed6324769684b6
adachiyutaka <adachi.yutaka@gmail.com>
2020-08-24 16:54:50 +0900

    保存処理のシンボル名をstageからstage_imgに変更

--- app/forms/game_form.rb
+++ app/forms/game_form.rb
@@ -1,16 +1,17 @@
 class GameForm
   include ActiveModel::Model
-  attr_accessor :stage, :player, :name, :text
+  attr_accessor :stage_img, :player_img, :name, :text, :user_id
 
   # with_options presence: true do
   # end
 
   def save
-    @game = Game.create(name: name, text: text, user_id: 1)
-    gameObject = GameObject.create(images: player, game_id: @game.id)
-    Position.create(x: 0, y: 0, game_object_id: gameObject.id)
-    stage = Stage.create(images: stage, game_id: @game.id)
+    @game = Game.create(name: name, text: text, user_id: user_id)
+    stage = Stage.create(image: stage_img, game_id: @game.id)
     Line.create(s_x: 0, s_y: 0, e_x: 0, e_y: 0, stage_id: stage.id)
+    gameObject = GameObject.create(image: player_img, game_id: @game.id)
+    Position.create(x: 0, y: 0, game_object_id: gameObject.id)
+    binding.pry
   end
 
   def game
