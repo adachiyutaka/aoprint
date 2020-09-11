09ca6ce625c86ed8c94f3f11cd0a21d7a48043f6
adachiyutaka <adachi.yutaka@gmail.com>
2020-09-11 13:15:32 +0900

    D&Dによる画像投稿機能の作成

--- app/javascript/create.js
+++ app/javascript/create.js
@@ -2,7 +2,7 @@ const create = () => {
   const form = document.getElementById('game_form');
   form.addEventListener('submit', (e) => {
     e.preventDefault;
-    var splitPlayer = document.getElementsByClassName('split-player')[1];
+    var splitPlayer = document.getElementsByClassName('split-img')[1];
     const renderDom = document.getElementById('game_form');
     const playerImg = `<input value=${splitPlayer.src} type='hidden' name='player_img'>`;
     renderDom.insertAdjacentHTML("beforeend", playerImg);
