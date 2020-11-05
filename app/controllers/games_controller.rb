7bae43a72ff6d4e9791b07d622601ffc4bbfe9b6
adachiyutaka <adachi.yutaka@gmail.com>
2020-11-05 18:19:04 +0900

    文字認識処理の試験

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,30 @@
 require 'base64'
 require 'json'
+require 'google/cloud/vision'
 
 class GamesController < ApplicationController
 
   def index
     @games = Game.all.order(created_at: 'DESC')
     @game = Game.new
+
+    #gcvテスト
+    image_annotator = Google::Cloud::Vision.image_annotator
+    image_path = 'public/images/test.jpg'
+
+    response = image_annotator.text_detection(
+      image:       image_path,
+    )
+    
+    result = Array.new
+    response.responses.each do |res|
+      res.text_annotations.each do |text|
+        result << text.bounding_poly.vertices[0].x
+        result << text.bounding_poly.vertices[0].y
+      end
+    end
+    @result = result
+
   end
 
   def show
