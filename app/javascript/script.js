a8461ddf765681cfa8bcffcb66cc5345dc6cff9d
adachiyutaka <adachi.yutaka@gmail.com>
2021-03-03 11:34:23 +0900

    script編集画面の仮組み作成

---
+++ app/javascript/script.js
@@ -0,0 +1,22 @@
+const script = () => {
+
+  // script_listの読み込み
+  // script_fieldの読み込み
+
+
+  // script_list
+
+  // scriptを追加する際のclickListenerを設定
+
+  // script_field
+
+  // 現在選択されているscript_card
+  let activeCard;
+
+  // script_cardを選択するためのclickLintenerを設定
+
+  // 全てのscript_cardの選択を解除し、クリックしたcardを再設定
+
+  //
+}
+window.addEventListener('load', script);
