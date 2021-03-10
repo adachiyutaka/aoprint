705074e932c6c7347df88ebdfb818e6a492141e9
adachiyutaka <adachi.yutaka@gmail.com>
2021-03-10 15:41:25 +0900

    game_objectテーブルにitem, goalカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_073449) do
+ActiveRecord::Schema.define(version: 2021_03_10_045944) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2021_02_09_073449) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "enemy", default: false
+    t.boolean "item", default: false
+    t.boolean "goal", default: false
     t.index ["game_id"], name: "index_game_objects_on_game_id"
   end
 
