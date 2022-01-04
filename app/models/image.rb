415980876a24187b735e629dc4f73079ce0c1c2f
adachiyutaka <adachi.yutaka@gmail.com>
2022-01-04 13:53:56 +0900

    imageモデルにattachされた画像ファイルの幅と高さを返すメソッドを追加

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,12 @@ class Image < ApplicationRecord
   has_one_attached :image
 
   belongs_to :game_object
+
+  def height
+    image.metadata['height']
+  end
+
+  def width
+    image.metadata['width']
+  end
 end
