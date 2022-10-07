c3b963ebf2377cd5cad3b84a6365cced00ad3eb6
adachiyutaka <adachi.yutaka@gmail.com>
2022-10-07 12:24:37 +0900

    不要なファイルを削除

--- app/javascript/createModal/splitImage.js
+++
@@ -1,7 +0,0 @@
-// import CreateController from './createController.js';
-// import GameObject from './gameObject.js';
-// import addImageList from './imageList';
-// import splitImageCV from './splitImageCV.js';
-
-
-// window.addEventListener('load', sendImage);
